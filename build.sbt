eae1d265cc99f3c24e3032632edfea65dcc38c8e
dbersenev <dmitry.bersenev@clarivate.com>
2019-06-05 14:19:07 +0400

    Removed http4s dep

--- build.sbt
+++ build.sbt
@@ -8,11 +8,10 @@ lazy val root = (project in file(".")).settings(
   commons,
   name := "fs2-xml",
   organization := "org.dbersenev",
-  version := "0.1.2",
+  version := "0.1.3",
   libraryDependencies ++= Seq(
     fs2Core, fs2Io, scalaXml, woodstox
   ),
-  libraryDependencies ++= http4s,
   scalacOptions += "-Ypartial-unification",
   mainClass in Compile := Some("org.dbersenev.fs2.xml.OrcidTest"),
   executableScriptName := "run",
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,13 +7,6 @@ object Dependencies{
   val scalaXml = "org.scala-lang.modules" %% "scala-xml" % "1.1.1"
 
   val scalaTags = "com.lihaoyi" %% "scalatags" % "0.6.7"
-
-  val http4sVersion = "0.19.0"
-  val http4s = Seq(
-    "org.http4s" %% "http4s-dsl" % http4sVersion,
-    "org.http4s" %% "http4s-blaze-server" % http4sVersion,
-    "org.http4s" %% "http4s-blaze-client" % http4sVersion
-  )
   
   val woodstox = "com.fasterxml.woodstox" % "woodstox-core" % "5.2.0"
 }
